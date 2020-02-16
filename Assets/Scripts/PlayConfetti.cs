6e53621c072fd724f5592dc888d0a798e3cecc87
tkhairullin <t.a.khairullin@gmail.com>
2020-02-17 04:15:05 +0600

    Add Project

---
+++ Assets/Scripts/PlayConfetti.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayConfetti : MonoBehaviour
+{
+    public GameObject ConfettiParticle;
+
+    public void PlVictory()
+    {
+        GameObject obj = Instantiate(ConfettiParticle);
+        Destroy(obj, 2.5f);
+    }
+}
